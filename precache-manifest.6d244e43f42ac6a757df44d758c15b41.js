2c4e25d1a5244a913fb2355d89aa90099c844471
Renato <renato145@hotmail.com>
2020-04-23 12:41:36 +0930

    Updates

--- precache-manifest.6d244e43f42ac6a757df44d758c15b41.js
+++ precache-manifest.6d244e43f42ac6a757df44d758c15b41.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "46b2a41d70781aa3e3c6e043093fa1bd",
+    "revision": "a49a8aed9637de3d84987a4c56311e5c",
     "url": "/fastexplorer-js/index.html"
   },
   {
-    "revision": "b63b92daa3cb5644996c",
+    "revision": "f3d8dcdbed4d38d4cafe",
     "url": "/fastexplorer-js/static/css/2.74189dbb.chunk.css"
   },
   {
-    "revision": "d8252bc6ef942a6df1df",
+    "revision": "245d9b0bdabbbb1b81b9",
     "url": "/fastexplorer-js/static/css/main.36497136.chunk.css"
   },
   {
-    "revision": "b63b92daa3cb5644996c",
-    "url": "/fastexplorer-js/static/js/2.95e1ae12.chunk.js"
+    "revision": "f3d8dcdbed4d38d4cafe",
+    "url": "/fastexplorer-js/static/js/2.8f50b834.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/fastexplorer-js/static/js/2.95e1ae12.chunk.js.LICENSE.txt"
+    "url": "/fastexplorer-js/static/js/2.8f50b834.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d8252bc6ef942a6df1df",
-    "url": "/fastexplorer-js/static/js/main.e11fcd1f.chunk.js"
+    "revision": "245d9b0bdabbbb1b81b9",
+    "url": "/fastexplorer-js/static/js/main.e71e8d7b.chunk.js"
   },
   {
     "revision": "598cf4fed7e6da629048",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fastexplorer-js/precache-manifest.32af4d26c70c6fc68f0e535e4e15abdb.js"
+  "/fastexplorer-js/precache-manifest.6d244e43f42ac6a757df44d758c15b41.js"
 );
 
 self.addEventListener('message', (event) => {
